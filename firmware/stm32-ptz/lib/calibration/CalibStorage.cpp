b7513b080d731a13a41c1113604773ed7368ddd0
Adcage <2042234720@qq.com>
2026-05-08 09:21:08 +0800

    重构代码结构，以提高可读性和可维护性

--- firmware/stm32-ptz/lib/calibration/CalibStorage.cpp
+++ firmware/stm32-ptz/lib/calibration/CalibStorage.cpp
@@ -2,12 +2,16 @@
 
 #include <EEPROM.h>
 
+extern "C" {
+#include <utility/stm32_eeprom.h>
+}
+
 namespace {
 constexpr int EEPROM_ADDR = 0;
 }  // namespace
 
 bool CalibStorage::load(CalibData& outData) {
-    EEPROM.begin();
+    eeprom_buffer_fill();
     EEPROM.get(EEPROM_ADDR, outData);
     if (outData.magic != MAGIC) {
         return false;
@@ -16,7 +20,19 @@ bool CalibStorage::load(CalibData& outData) {
 }
 
 bool CalibStorage::save(const CalibData& data) {
-    EEPROM.begin();
+    eeprom_buffer_fill();
     EEPROM.put(EEPROM_ADDR, data);
-    return true;
+    eeprom_buffer_flush();
+
+    CalibData written{};
+    eeprom_buffer_fill();
+    EEPROM.get(EEPROM_ADDR, written);
+
+    return written.magic == data.magic &&
+           written.panMin == data.panMin &&
+           written.panMax == data.panMax &&
+           written.panCenter == data.panCenter &&
+           written.tiltMin == data.tiltMin &&
+           written.tiltMax == data.tiltMax &&
+           written.tiltCenter == data.tiltCenter;
 }
